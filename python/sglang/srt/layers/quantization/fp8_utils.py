527937be227ea9aef3686a2b11f64543acbf32df
seungrokj <seungrok.jung@amd.com>
2026-03-04 04:17:15 +0000

    aiter gemm a8w8 configure
    Signed-off-by: seungrokj <seungrok.jung@amd.com>

--- python/sglang/srt/layers/quantization/fp8_utils.py
+++ python/sglang/srt/layers/quantization/fp8_utils.py
@@ -53,6 +53,7 @@ _is_fp8_fnuz = is_fp8_fnuz()
 
 _use_aiter = get_bool_env_var("SGLANG_USE_AITER") and _is_hip
 
+
 def is_aiter_triton_gemm_w8a8_tuned(n: int, k: int) -> bool:
     return (n, k) in [
         (1024, 8192),
@@ -70,13 +71,16 @@ def is_aiter_triton_gemm_w8a8_tuned(n: int, k: int) -> bool:
         (8192, 32768),
     ]
 
+
 if _use_aiter:
     import aiter
 
     # from aiter import gemm_a8w8_blockscale, gemm_a8w8_bpreshuffle, get_hip_quant
-    from aiter import gemm_a8w8_bpreshuffle, get_hip_quant
-    from aiter.ops.triton.gemm_a8w8_blockscale import gemm_a8w8_blockscale as triton_gemm_a8w8_blockscale
     from aiter import gemm_a8w8_blockscale as gemm_a8w8_blockscale
+    from aiter import gemm_a8w8_bpreshuffle, get_hip_quant
+    from aiter.ops.triton.gemm_a8w8_blockscale import (
+        gemm_a8w8_blockscale as triton_gemm_a8w8_blockscale,
+    )
 
     aiter_per1x128_quant = get_hip_quant(aiter.QuantType.per_1x128)
 
@@ -697,10 +701,7 @@ def aiter_w8a8_block_fp8_linear(
 
     n, k = weight.shape
 
-    use_triton = (
-        not _is_fp8_fnuz
-        and is_aiter_triton_gemm_w8a8_tuned(n, k)
-    )
+    use_triton = not _is_fp8_fnuz and is_aiter_triton_gemm_w8a8_tuned(n, k)
 
     if use_triton:
         gemm_a8w8_blockscale_op = triton_gemm_a8w8_blockscale
